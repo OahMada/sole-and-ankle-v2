be271ced879805449bf0d9bf4c8b0cd69f113fa9
Adam Hao <haozg44@outlook.com>
2025-03-10 18:50:53 +0800

    finish implementing the third exercise

--- src/components/ShoeIndex/ShoeIndex.jsx
+++ src/components/ShoeIndex/ShoeIndex.jsx
@@ -10,49 +10,59 @@ import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
 const ShoeIndex = ({ sortId, setSortId }) => {
-  return (
-    <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={34} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<MainColumn>
+				<Header>
+					<Title>Running</Title>
+					<Select label='Sort' value={sortId} onChange={(ev) => setSortId(ev.target.value)}>
+						<option value='newest'>Newest Releases</option>
+						<option value='price'>Price</option>
+					</Select>
+				</Header>
+				<Spacer size={34} />
+				{/* <ShoeGrid /> */}
+			</MainColumn>
+			<LeftColumn>
+				<Breadcrumbs>
+					<Breadcrumbs.Crumb href='/'>Home</Breadcrumbs.Crumb>
+					<Breadcrumbs.Crumb href='/sale'>Sale</Breadcrumbs.Crumb>
+					<Breadcrumbs.Crumb href='/sale/shoes'>Shoes</Breadcrumbs.Crumb>
+				</Breadcrumbs>
+				<Spacer size={42} />
+				<ShoeSidebar />
+			</LeftColumn>
+		</Wrapper>
+	);
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: row-reverse;
+	gap: 32px;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	flex: 0 0 250px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+	flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+	display: flex;
+	align-items: center;
+`;
 
 const Title = styled.h2`
-  font-size: 1.5rem;
-  font-weight: ${WEIGHTS.medium};
+	font-size: 1.5rem;
+	font-weight: ${WEIGHTS.medium};
+
+	margin-right: auto;
 `;
 
 export default ShoeIndex;
